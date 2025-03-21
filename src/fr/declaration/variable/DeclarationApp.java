829ea3a08f93a6aea36a4aee4d9ae3b439d54302
Cyril29D <cyrildelly9@gmail.com>
2025-03-21 12:20:53 +0100

    tp 3

---
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,20 @@
+package fr.declaration.variable;
+
+public class DeclarationApp {
+    public static void  main(String[] args){
+        byte b = 10 ;
+        short s = 20 ;
+        int i = 30 ;
+        long l = 1000l ;
+        double d = 1.2 ;
+        char ch = 'C';
+        boolean False;
+   String St = "Voici le résultat d'un calcul : 1+5=6";
+System.out.println(St);
+
+
+
+
+
+    }
+}
